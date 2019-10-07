631e2e2d676676fa78a4b1b9ac58dd90b5f7ad50
José Maria Martín Vitero <jose.martin@atmira.com>
2019-10-07 12:52:11 +0200

    Pruebas con clase Coche

--- Program.cs
+++ Program.cs
@@ -9,11 +9,14 @@ namespace temp
             int var1 = 2;
             int var2 = 4;
             Circulo miCirculo = new Circulo();
+            Coche Coche01 = new Coche(17.00);
 
             Console.WriteLine(" El resultado de la suma es: " + (var1 + var2));
             System.Console.WriteLine($"La variable 1 es:{var1}");
 
             miCirculo.calculoArea(4);            
+
+            System.Console.WriteLine(Coche01.getInfoCoche());
         }
     }
 
@@ -21,6 +24,20 @@ namespace temp
         const double pi = 3.1416;
         public double calculoArea(int radio){
             return  pi * radio * radio;
+        }
+    }
+
+    class Coche{
+        private int ruedas;
+        private double  largo;
+
+        public Coche (double larcgoCoche){
+            ruedas = 4;
+            this.largo = larcgoCoche;
+        }
+
+        public String getInfoCoche(){
+            return $"La información del coche es: Medidas={ruedas} y largo={largo}";
         }
     }
 }
