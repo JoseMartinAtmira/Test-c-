b8465a799ff7b5082a61aa00304b86a34b3f466f
JoseMartinAtmira <56254229+JoseMartinAtmira@users.noreply.github.com>
2019-10-07 12:00:50 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace temp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int var1 = 2;
+            int var2 = 4;
+            Circulo miCirculo = new Circulo();
+
+            Console.WriteLine(" El resultado de la suma es: " + (var1 + var2));
+            System.Console.WriteLine($"La variable 1 es:{var1}");
+
+            miCirculo.calculoArea(4);            
+        }
+    }
+
+    class Circulo{
+        const double pi = 3.1416;
+        public double calculoArea(int radio){
+            return  pi * radio * radio;
+        }
+    }
+}
